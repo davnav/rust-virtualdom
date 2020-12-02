6c22d9899b26e83d6bbfb0d322b6ecc627eeb06e
naveen <naveendavisv@gmail.com>
2020-12-01 22:00:20 -0800

    check html_parser crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,59 @@
 ///How to write a Virtual Dom in Rust
 ///inspired from https://medium.com/@deathmood/how-to-write-your-own-virtual-dom-ee74acc13060
+/// 
+use html_parser::Dom; 
 
+/// 
+/// Sample DOM elements like
+/// { tag:'..', props: '..' ,childern: [..] }
+/// 
+/// DOM text nodes would be plain Strings
+/// 
+///
 
+///Virtual DOM
 pub struct VirtualDom{
-    nodes:Vec<Node>,
+    parent:Node,
+//    nodes:Vec<Box<Node>>,
 }
 
+impl VirtualDom{
+    fn new(root:Node) ->Self{
+        VirtualDom{
+            parent:root,
+        }
+    }
+}
+
+static html:&str = "<ul class='list'>
+                        <li>item1</li>
+                        <li>item2</li>
+                     </ul>";
 pub struct Node{
     tag:String,
     props:Vec<String>,
-    chirdren:Vec<Node>,
+    chirdren:Vec<Box<Node>>,
 }
+
+
+fn parseNode(htl:&str) {
+
+    let dm = Dom::parse(&htl);
+    println!("{:?}",dm);
+
+
+
+}
+
+
 
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        parseNode(html);
+    
     }
 }
