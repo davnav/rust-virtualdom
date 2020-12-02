69dbe052951bbb62ce2749cc1637339c04f48ee3
naveen <naveendavisv@gmail.com>
2020-12-01 19:13:27 -0800

    remove target' '

--- target/debug/deps/virtualdom-6b60e996c5aa342c.d
+++
@@ -1,5 +0,0 @@
-/home/naveen/rustprojects/virtualdom/target/debug/deps/virtualdom-6b60e996c5aa342c.rmeta: src/lib.rs
-
-/home/naveen/rustprojects/virtualdom/target/debug/deps/virtualdom-6b60e996c5aa342c.d: src/lib.rs
-
-src/lib.rs:
